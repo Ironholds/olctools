ef7482a293c5905f58ff61dc40c8c5bb5f7af1ce
ironholds <ironholds@gmail.com>
2015-05-31 22:01:58 -0400

    some unit tests

--- tests/testthat/test_validators.R
+++ tests/testthat/test_validators.R
@@ -1,5 +1,17 @@
 context("Test OLC validation")
 
-test_that("Obviously invalid OLCs can be identified", {
+test_that("Strings with illegal characters can be identified", {
   expect_that(valid_olc("this has an invalid character. Lots!"), equals(FALSE))
+})
+
+test_that("Strings missing a separator can be identified", {
+  expect_that(valid_olc("G"), equals(FALSE))
+})
+
+test_that("Strings with multiple separators can be identified", {
+  expect_that(valid_olc("GG+C+"), equals(FALSE))
+})
+
+test_that("Strings with separators in the wrong place can be identified", {
+  expect_that(valid_olc("GG2+"), equals(FALSE))
 })
