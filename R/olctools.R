fc5d882059508078c88ee281b4a26a51ca1b592a
ironholds <ironholds@gmail.com>
2015-08-05 23:54:21 -0400

    add vignette

--- R/olctools.R
+++ R/olctools.R
@@ -1,6 +1,7 @@
 #' @title Tools for handling Open Location Codes
 #' @name olctools
-#' @description blah dee blah dee blah
+#' @description \href{http://openlocationcode.com/}{Open Location Codes} are a Google-created standard for identifying geographic locations.
+#' olctools provides utilities for validating, encoding and decoding entries that follow this standard.
 #' @useDynLib olctools
 #' @importFrom Rcpp sourceCpp
 #' @docType package
